f820676b65bdff423914aef83c565ed215162127
anatoly techtonik <techtonik@gmail.com>
2016-05-05 15:33:29 +0300

    List all *.ion files in current directory

---
+++ cmdline/02listfiles.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import glob
+
+for filename in glob.glob('*.ion'):
+    print(filename)
---
+++ cmdline/02listfiles.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# transpilery:primary
+
+for filename in *.ion
+do
+  echo "$filename"
+done
